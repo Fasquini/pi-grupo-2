650a524a8926ca381d85ec70939f8ea4cef72186
AnaCNunez <anacnunezibanez@gmail.com>
2026-04-20 10:32:11 -0300

    Logout

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,52 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
 import Cookies from "universal-cookie";
+import { Link, withRouter } from "react-router-dom";
 import LiNavbar from "../LiNavbar/LiNavbar";
 const cookie = new Cookies()
 
-function Navbar() {
-    let menu = cookie.get("user-auth-cookie") !== undefined ? [
-        { name: "Home", path: "/", clase: "" },
-        { name: "Populares", path: "/Populares", clase: "" },
-        { name: "Top Rated", path: "/TopRated", clase: "" },
-        { name: "Pronto", path: "/Pronto", clase: "" },
-        { name: "Favoritos", path: "/Favoritas", clase: "" } 
-    ]
-    : [
-        { name: "Home", path: "/", clase: "" },
-        { name: "Populares", path: "/Populares", clase: "" },
-        { name: "Top Rated", path: "/TopRated", clase: "" },
-        { name: "Pronto", path: "/Pronto", clase: "" }
-    ]
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            datos: "",
+        };
+    }
     
+    logOut() { 
+        cookie.remove("user-auth-cookie") 
+        this.props.history.push("/");
+    }
 
-    let lr = [
-        { name: "Registro", path: "/Registro", clase: "Registro" },
-        { name: "Login", path: "/Login", clase: "Login" }
-    ];
+    render() {
+        let menu = cookie.get("user-auth-cookie") !== undefined ? [
+            { name: "Home", path: "/", clase: "" },
+            { name: "Populares", path: "/Populares", clase: "" },
+            { name: "Top Rated", path: "/TopRated", clase: "" },
+            { name: "Pronto", path: "/Pronto", clase: "" },
+            { name: "Favoritos", path: "/Favoritas", clase: "" }
+        ]
+            : [
+                { name: "Home", path: "/", clase: "" },
+                { name: "Populares", path: "/Populares", clase: "" },
+                { name: "Top Rated", path: "/TopRated", clase: "" },
+                { name: "Pronto", path: "/Pronto", clase: "" }
+            ]
 
-    let usuario = cookie.get("user-auth-cookie");
 
-    let logOut = ()=>{cookie.remove("user-auth-cookie")}
+        let lr = [
+            { name: "Registro", path: "/Registro", clase: "Registro" },
+            { name: "Login", path: "/Login", clase: "Login" }
+        ];
 
-    return (
-        <nav>
-            <ul className="">
-                <LiNavbar name={menu} />
-            </ul>
+        let usuario = cookie.get("user-auth-cookie");
 
-            <ul>
-                {usuario === undefined ? (
-                    <LiNavbar name={lr} />
-                ) : (
-                    <li className="usuarioLogueado">
-                        <img src="https://img.icons8.com/?size=100&id=82792&format=png&color=CC0A0A" onClick={logOut} className="logOut" alt="foto de perfil"/>
-                        <img src="https://img.icons8.com/?size=100&id=85050&format=png&color=000000" className="fotoDePerfil" alt="foto de perfil"/>
-                        <p>{usuario}</p>
-                    </li>
-                )}
-            </ul>
-        </nav>
-    );
+
+        return (
+            <nav>
+                <ul className="">
+                    <LiNavbar name={menu} />
+                </ul>
+
+                <ul>
+                    {usuario === undefined ? (
+                        <LiNavbar name={lr} />
+                    ) : (
+                        <li className="usuarioLogueado">
+                            <img src="https://img.icons8.com/?size=100&id=82792&format=png&color=CC0A0A" onClick={() => this.logOut()} className="logOut" alt="foto de perfil" />
+                            <img src="https://img.icons8.com/?size=100&id=85050&format=png&color=000000" className="fotoDePerfil" alt="foto de perfil" />
+                            <p>{usuario}</p>
+                        </li>
+                    )}
+                </ul>
+            </nav>
+        );
+    }
+
 }
 
-export default Navbar;
+export default withRouter(Navbar);
